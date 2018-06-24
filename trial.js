d9116feada7ad8f3a0daebcf5fe1e54187ceb41c
bizzy <blopes1989@gmail.com>
2018-06-23 21:58:23 -0400

    buttons work

--- trial.js
+++ trial.js
@@ -4,15 +4,13 @@ $( document ).ready(function() {
 
     function renderButtons() {
 
-        // Deleting the movie buttons prior to adding new movie buttons
-        // (this is necessary otherwise we will have repeat buttons)
-        $("#searchbuttons").empty();
+        // Deleting the buttons prior to adding new buttons
+        $(".searchbuttons").empty();
 
-        // Looping through the array of movies
+        // Looping through the array
         for (var i = 0; i < sarray.length; i++) {
 
-          // Then dynamicaly generating buttons for each movie in the array.
-          // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
+          // Then dynamicaly generating buttons for each item array.
           var a = $("<button>");
           // Adding a class
           a.addClass("btn btn-primary");
@@ -21,40 +19,26 @@ $( document ).ready(function() {
           // Providing the button's text with a value of the movie at index i
           a.text(sarray[i]);
           // Adding the button to the HTML
-          $("#searchbuttons").append(a);
+          $(".searchbuttons").append(a);
         }}
-        renderButtons();
-
-    console.log(sarray)
-    for  (var i = 0; i < sarray.length; i++) {
-        var buttonz = $("<button>");
-        buttonz.addClass("btn btn-primary")
-        buttonz.attr("clickable")
-        buttonz.text(sarray[i])
-        buttonz.attr("data-name", sarray[i])
 
 
-        $(".searchbuttons").append(buttonz)
-    };
+renderButtons();
 
 
 
     $("#submitted").on("click", function(event) {
         // event.preventDefault() prevents the form from trying to submit itself.
-        // We're using a form so that the user can hit enter instead of clicking the button if they want
         event.preventDefault();
 
         // This line will grab the text from the input box
         var sport = $("#added").val().trim();
-        // The movie from the textbox is then added to our array
+        // The word from the textbox is then added to our array
         sarray.push(sport);
         console.log(sarray)
-        // calling renderButtons which handles the processing of our movie array
-      //  renderButtons();
-      });
-
-      // Calling the renderButtons function at least once to display the initial list of movies
+        // calling renderButtons which handles the processing of array
       renderButtons();
+      });
 
             // displayMovieInfo function re-renders the HTML to display the appropriate content
             function displayGif() {
@@ -71,11 +55,11 @@ $( document ).ready(function() {
                   // Creating a div to hold the movie
                   var gif = $("<div class='gifDiv'>");
                   gif=response
-                  
+
                 });
         console.log(queryURL)
               }
-              $(document).on("click", "#searchbuttons", displayGif);
+              $(document).on("click", ".searchbuttons", displayGif);
         
 
 
